3a3a6a6f06493fea184c61a8ab63ad44f777fe13
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2019-08-19 23:13:55 +0900

    login画面のレイアウト

--- WarakasuApp/Login/LoginViewController.swift
+++ WarakasuApp/Login/LoginViewController.swift
@@ -19,18 +19,18 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passWordTextfiled: UITextField!
     
     // 新規登録ボタン
-    @IBAction func newMember(_ sender: Any) {
+    @IBAction func newMember(_ sender: TransitionButton) {
         //emailとパスワードにnilが入っていないかを確認
         guard let email = emailTextFiled.text, let password = passWordTextfiled.text else {
             return
         }
-        
         sender.startAnimation()
-        
-        // ログイン処理
+        // firebaseuthのログイン処理
         Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
             if let error = error {
                 self.showErrorAlert(error: error)
+                // 認証失敗
+                print("新規作成失敗")
                 sender.stopAnimation()
             } else {
                 let qualityOfServiceClass = DispatchQoS.QoSClass.background
@@ -42,44 +42,10 @@ class LoginViewController: UIViewController {
                     DispatchQueue.main.async(execute: { () -> Void in
                         
                         sender.stopAnimation(animationStyle: .expand, completion: {
-                            // 成功
+                            // 認証成功
+                            print("新規作成成功")
                             UserDefaults.standard.set("check", forKey: "set")
                             self.toTimeLine()
-                            //                            self.setupKobiraViewController()
-                        })
-                    })
-                }
-            }
-        })
-    }
-    // サインイン
-    @IBAction func loginbutton(_ sender: Any) {
-        //emailとパスワードにnilが入っていないかを確認
-        guard let email = emailTextFiled.text, let password = passWordTextfiled.text else {
-            return
-        }
-        
-        sender.startAnimation()
-        
-        // ログイン処理
-        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
-            if let error = error {
-                self.showErrorAlert(error: error)
-                sender.stopAnimation()
-            } else {
-                let qualityOfServiceClass = DispatchQoS.QoSClass.background
-                
-                let backgroundQueue = DispatchQueue.global(qos: qualityOfServiceClass)
-                
-                backgroundQueue.async {
-                    
-                    DispatchQueue.main.async(execute: { () -> Void in
-                        
-                        sender.stopAnimation(animationStyle: .expand, completion: {
-                            // 成功
-                            UserDefaults.standard.set("check", forKey: "set")
-                            self.toTimeLine()
-                            //                            self.setupKobiraViewController()
                         })
                     })
                 }
@@ -87,7 +53,55 @@ class LoginViewController: UIViewController {
         })
     }
     
-    // エラーが返ってきた場合のアラーと
+    
+    // サインインボタン
+    @IBAction func loginbutton(_ sender: TransitionButton) {
+        //emailとパスワードにnilが入っていないかを確認
+        guard let email = emailTextFiled.text, let password = passWordTextfiled.text else {
+            return
+        }
+        
+        sender.startAnimation()
+        
+        // ログイン処理
+        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+            if let error = error {
+                self.showErrorAlert(error: error)
+                // 認証の失敗
+                print("ログイン失敗")
+                sender.stopAnimation()
+            } else {
+                let qualityOfServiceClass = DispatchQoS.QoSClass.background
+                
+                let backgroundQueue = DispatchQueue.global(qos: qualityOfServiceClass)
+                
+                backgroundQueue.async {
+                    
+                    DispatchQueue.main.async(execute: { () -> Void in
+                        
+                        sender.stopAnimation(animationStyle: .expand, completion: {
+                            // 成功
+                            print("ログイン成功")
+                            UserDefaults.standard.set("check", forKey: "set")
+                             // タイムラインへ遷移
+                            self.toTimeLine()
+                        })
+                    })
+                }
+            }
+        })
+    }
+    
+    func toTimeLine() {
+        // storyboardのfileの特定
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        // 移動先のvcをインスタンス化
+        let vc = storyboard.instantiateViewController(withIdentifier: "Main")
+        // 遷移処理
+        self.present(vc, animated: true)
+    }
+    
+    // エラーが返ってきた場合のアラート
     func showErrorAlert(error: Error?)  {
         let alert = UIAlertController(title: "エラー", message: "error?", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .cancel)
@@ -96,6 +110,8 @@ class LoginViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
+    // キーボードを閉じる処理
+    // 画面が触られたかを判断
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // キーボードが開いていたら
         if emailTextFiled.isFirstResponder {
@@ -107,10 +123,4 @@ class LoginViewController: UIViewController {
         }
     }
     }
-    
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//    }
 
