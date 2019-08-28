3f1e32a2262f0dc63ffb1aa0e450235d1bfad5ef
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2019-08-28 13:26:57 +0900

    タイムラインに動画を投稿できる機能の追加

---
+++ WarakasuApp/TabMake/TimeLine/Gyagu/GyaguTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GyaguTableViewCell.swift
+//  WarakasuApp
+//
+//  Created by 志賀大河 on 2019/08/28.
+//  Copyright © 2019 Taigashiga. All rights reserved.
+//
+
+import UIKit
+
+class GyaguTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WarakasuApp/TabMake/TimeLine/konto/ConteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConteTableViewCell.swift
+//  WarakasuApp
+//
+//  Created by 志賀大河 on 2019/08/28.
+//  Copyright © 2019 Taigashiga. All rights reserved.
+//
+
+import UIKit
+
+class ConteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
